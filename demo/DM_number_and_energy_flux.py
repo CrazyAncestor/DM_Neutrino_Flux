dbb6bd103966e54a66f34fb1ce89f9b1af046287
Albert <colonist288@gmail.com>
2020-08-03 10:55:13 +0800

    Correct DM_flux

--- demo/DM_number_and_energy_flux.py
+++ demo/DM_number_and_energy_flux.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import DM_p_distribution as dmp
 import scipy.integrate as integrate
-import pynverse as vs
+
 
 #Particle Property
 #Neutrino
@@ -13,7 +13,7 @@ E_per_nu = 10e6 #Mean energy of each neutrino #estimated value
 Alpha = 2.0 #Energy Distribution Parameter
 
 #DM
-M_DM = 1e03
+M_DM = 1e06
 E_max = 60e06
 
 #NFW Parameter
@@ -24,22 +24,29 @@ rs=24.42*3.08567758e21
 t_burst = 10*3e10
 
 #cross section
-cs = 1e-30
+cs = 1e-28
 
 #SN
 Ri=1e7
 
 def DM_flux(m_dm,e_max,e_per_nu,alpha,start,end,n_total):
-    L = (np.sum((start-end)**2))**0.5
-    l = end -start
-    r=(np.sum((start)**2))**0.5
-    x= r/rs
-    
-    result = rho_s*n_total*cs/(x*(1+x)*(1+x)) /( 4*np.pi*m_dm*Ri)
+    gamma = dmp.energy_kicked_by_neutrino(E_per_nu, M_nu,m_dm)/m_dm
+    beta = (1-gamma**(-2))**0.5
+    time_delay = np.sum((start-end)**2)**0.5*(1/beta-1)/(3e10)
+    print("time delay:"+str(time_delay))
 
-    r_nu=n_total/(4*np.pi*L*L)
-    print("DM Flux:"+str(result))
-    print("Neutrino Flux:"+str(r_nu))
+    R = (np.sum((start-end)**2))**0.5
+    l = end -start
+    def f(t):
+        r=(np.sum((start+l*t)**2))**0.5
+        x= r/rs
+        return 1/(x*(1+x)*(1+x))
+    k = n_total*rho_s*cs/m_dm/(4*np.pi*(R**2)) *R
+    
+    phi_dm = integrate.nquad(f, [[0,1.]])[0]*k
+    phi_nu = n_total/(4*np.pi*R*R)
+    print("DM Flux:"+str(phi_dm))
+    print("Neutrino Flux:"+str(phi_nu))
     
 if __name__== '__main__':
     
@@ -49,10 +56,7 @@ if __name__== '__main__':
     start=np.array([1*3.08567758e21,0*3.08567758e21,0])
     end =np.array([10*3.08567758e21,0,0])
     
-    #DM_flux(M_DM,E_max,E_per_nu ,Alpha,start,end,E_total_nu/E_per_nu)
+    DM_flux(M_DM,E_max,E_per_nu ,Alpha,start,end,E_total_nu/E_per_nu)
 
-    gamma = dmp.energy_kicked_by_neutrino(E_per_nu, M_nu,M_DM)/M_DM
-    beta = (1-gamma**(-2))**0.5
-    time_delay = np.sum((start-end)**2)**0.5*(1/beta-1)/(3e10)
-    print(time_delay)
+    
     
