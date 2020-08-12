f977d193d62f4e57d3671260123d345d2233d51e
Albert <colonist288@gmail.com>
2020-08-12 16:20:48 +0800

    Kinematic DM flux

--- demo/DM_flux.py
+++ demo/DM_flux.py
@@ -21,6 +21,40 @@ rs=24.42*3.08567758e21
 #cross section (Neutrino and DM)
 cs = 1e-28
 
+def DM_flux(m_dm,e_per_nu,start,end,n_total):
+    gamma = kim.energy_kicked_by_neutrino(E_per_nu, M_nu,m_dm)/m_dm
+    beta = (1-gamma**(-2))**0.5
+    R = (np.sum((start-end)**2))**0.5
+    pos = -start+end
+    time_delay = R*(1/beta-1)/3e10
+    print("time delay(s):"+str(time_delay))
+
+    T = R/3e10
+    t = np.linspace(0,time_delay,100)
+    
+    def n_ori(l):
+        
+        def get_mod(x):
+            x2 = x**2
+            return (x2[:,0] + x2[:,1] +x2[:,2])**0.5
+        
+        r= get_mod(np.tensordot(l,pos,axes=0)/R+np.tensordot(np.ones(l.shape),start,axes=0))
+        
+        x= r/rs
+        return 1/(x*(1+x)*(1+x))
+
+    def n(t):
+        l = 3e10*(T - beta*t/(1-beta))
+        l[-1]= 0.
+        return n_ori(l)
+    phi = cs *n_total*rho_s/m_dm/(4*np.pi*(R**2))*n(t) *beta/(1-beta)
+    plt.plot(t, phi, color ='blue', label = 'DM Flux')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Flux (#/cm^2*s)')
+    plt.legend(loc='upper right')
+    
+    plt.show()
+    
 
 def DM_number(m_dm,e_per_nu,start,end,n_total):
     gamma = kim.energy_kicked_by_neutrino(E_per_nu, M_nu,m_dm)/m_dm
@@ -51,6 +85,6 @@ if __name__== '__main__':
     end =np.array([8.7*3.08567758e21,0,24*3.08567758e18])
     
     DM_number(M_DM,E_per_nu ,start,end,E_total_nu/E_per_nu)
-
+    DM_flux(M_DM,E_per_nu ,start,end,E_total_nu/E_per_nu)
     
     
