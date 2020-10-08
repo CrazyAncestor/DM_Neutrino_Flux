995003118be7db965f975827307ab01ad4157332
Albert <colonist288@gmail.com>
2020-10-08 14:03:50 +0800

    Simplification

--- demo/Integration_phi.py
+++ demo/Integration_phi.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
 e = 1.
-
+M_chi = 0.1e09
+sigma_chi = 1e-30
 def differential_intensity_array(R,i):
     split = 0
     for k in range(len(R)):
@@ -44,6 +45,8 @@ def differential_intensity(R,i):
             Rb = R
             DIb = 3120 -5530/Rb +3370/(1.29+Rb) +134000/(88.5+Rb) -1170000/(861+Rb) + 0.03*Rb
             return DIb/(R**2.7)
+def dR_dI(R):
+    return differential_intensity(R,0)
             
 def phi_T(T,i):
     m = 0
@@ -59,7 +62,7 @@ def phi_T(T,i):
     DI_DR = differential_intensity(R*1e-9,i)
     return 4*np.pi*(T+m)/(Z*e*((T**2+2*m*T)**0.5))*DI_DR
 
-def int_base(T,i,M_chi):
+def int_base(T,i):
 
     if i==0:#Proton
         m = 938.08e06
@@ -71,39 +74,47 @@ def int_base(T,i,M_chi):
     T_chi_max = (T**2+2*m*T)/(T+ (m+M_chi)**2/(2*M_chi) )
     return phi_T(T,i)/T_chi_max
 
-def integration(T_chi,i,M_chi):
+def integration(T_chi,i):
     T_min = (T_chi/2-m)* (1-(1+ 2*T_chi*((m+M_chi)**2) /(M_chi* ((2*m-T_chi)**2)) )**0.5)
-    result = quad(int_base, T_min, T_min*10, args=(i,M_chi))
+    result = quad(int_base, T_min, T_min*1000, args=(i))
     
     #print(T_min)
     #print(result)
     return result[0]
 
-def phi_chi_dt(T_chi,i,M_chi,sigma_chi,lumda):
+def phi_chi_dt(T_chi,i,lumda):
     def G(q2):
         return 1/((1+q2/(lumda**2))**2)
-    return sigma_chi*(G(2*M_chi*T_chi)**2)*integration(T_chi,i,M_chi)
+    return sigma_chi*(G(2*M_chi*T_chi)**2)*integration(T_chi,i)
 
-if __name__== '__main__':
-    T_chi = 10e6
+def phi_result(T_chi):
+    
     proton = 0
     helium = 1
-    m = 938.08e06
-    M_chi = 0.1e09
     
-    sigma_chi = 1e-30
     lumda_proton = 770e06
     lumda_helium = 410e06
+    mem1 = phi_chi_dt(T_chi,proton,lumda_proton)
+    mem2 = phi_chi_dt(T_chi,helium,lumda_helium)
+    return T_chi*(mem1+mem2)
 
-    logT = np.linspace(5,7.5,100)
-    T = np.exp(logT*np.log(10))
-    dphi_dT =[]
-    for k in range(len(T)):
-        mem1 = phi_chi_dt(T[k],proton,M_chi,sigma_chi,lumda_proton)
-        mem2 = phi_chi_dt(T[k],helium,M_chi,sigma_chi,lumda_helium)
-        dphi_dT.append(mem2)
-    dphi_dT = T*np.array(dphi_dT)
-    log_dphi_dT = np.log(dphi_dT)/np.log(10)
-    plt.plot(logT, log_dphi_dT, color ='blue')
+def show_plot(logx,func):
+    
+    x = np.exp(logx*np.log(10))
+    y = []
+    for k in range(len(x)):
+        y.append(func(x[k]))
+    y = np.array(y)
+    logy = np.log(y/np.log(10))
+    
+    plt.plot(logx, logy, color ='blue')
     plt.show()
-    #integration(T_chi,proton,M_chi)
+
+if __name__== '__main__':
+
+    logT_chi = np.linspace(4,8,100)
+
+    logR = np.linspace(-1,2,100)
+    #show_plot(logT_chi,phi_result)
+    show_plot(logR,dR_dI)
+    #integration(T_chi,proton)
