93ff19509c70b60cb507fbefadeadb82190073e9
Albert <colonist288@gmail.com>
2021-02-22 13:53:36 +0800

    Angle_Effect

--- demo/Angle_Effect.py
+++ demo/Angle_Effect.py
@@ -2,7 +2,7 @@
 import numpy as np
 import math as mt
 import matplotlib.pyplot as plt
-import Kinematics as kim
+import Kinematics 
 import scipy.integrate as integrate
 
 
@@ -25,7 +25,7 @@ cs = 1e-30
 def angle_norm(beta,r,R):
     def f(psi):
         rp = (R**2+r**2-2*R*r*mt.cos(psi))**0.5
-        cos_theta_psi = ((R**2+(r*mt.cos(psi))**2-2*R*r*mt.cos(psi))/(R**2+r**2-2*R*r*mt.cos(psi)))**0.5#(rp**2+R**2-r**2)/(2*R*rp)
+        cos_theta_psi = ((R**2+(r*mt.cos(psi))**2-2*R*r*mt.cos(psi))/(R**2+r**2-2*R*r*mt.cos(psi)))**0.5
         
         def g(psi):
             def theta(psi):
@@ -44,32 +44,34 @@ def angle_norm(beta,r,R):
     return integrate.nquad(f, [[0,psi_max]])[0] 
 
 def DM_number(m_dm,e_per_nu,start,end,n_total):
-    gamma = Kinematics.energy_kicked_by_neutrino(E_per_nu, M_nu,m_dm)/m_dm
-    beta = (1-gamma**(-2))**0.5
+    beta = (E_per_nu**2- M_nu**2)**0.5/(E_per_nu+M_DM)
+    gamma = (1-beta**(2))**0.5  
     time_delay = np.sum((start-end)**2)**0.5*(1/beta-1)/(3e10)
-    #print("time delay:"+str(time_delay))
+    
 
     R = (np.sum((start-end)**2))**0.5
     l = end -start
-    def f(psi,r):
-        
+    def f(r,theta):
+        psi = theta - mt.asin(r/R*mt.sin(theta))
+        dpsi_dtheta = 1- r/R*mt.cos(theta) /(1-(r/R*mt.sin(theta))**2)**0.5
         x=(np.sum((start + np.array([r*mt.cos(psi),0,r*mt.sin(psi)]))**2))**0.5/rs
         cos_theta_psi=((R**2+(r*mt.cos(psi))**2-2*R*r*mt.cos(psi))/(R**2+r**2-2*R*r*mt.cos(psi)))**0.5
         theta = mt.atan(1/(1/(mt.tan(psi))-r/(R*mt.sin(psi))))
         sec = 1/mt.cos(theta)
         dn_domega= (sec**3)*(1-beta*beta)/((sec**2-beta**2)**2)/np.pi
         
-        return mt.sin(psi)*cos_theta_psi /(R**2+r**2-2*R*r*mt.cos(psi))*dn_domega/(x*(1+x)*(1+x))
+        return mt.sin(psi)*cos_theta_psi *dpsi_dtheta *R/(R**2+r**2-2*R*r*mt.cos(psi))*dn_domega/(x*(1+x)*(1+x))
         
     k = n_total*rho_s*cs/m_dm/(4*np.pi)
-    
-    L_dm = integrate.dblquad(f, 0, R, lambda r: 0, lambda r: np.pi/2.-mt.asin(r/R))[0]*2*np.pi*k
+    result =integrate.dblquad(f, 0, np.pi/2., lambda theta: 0, lambda theta: R)
+    print(result[1])
+    L_dm = result[0]*2*np.pi*k/R
     print("DM Number(1/cm^2):"+str(L_dm))
     
 
 def DM_number_original(m_dm,e_per_nu,start,end,n_total):
-    gamma = Kinematics.energy_kicked_by_neutrino(E_per_nu, M_nu,m_dm)/m_dm
-    beta = (1-gamma**(-2))**0.5
+    beta = (E_per_nu**2- M_nu**2)**0.5/(E_per_nu+M_DM)
+    gamma = (1-beta**(2))**0.5  
     time_delay = np.sum((start-end)**2)**0.5*(1/beta-1)/(3e10)
     
 
@@ -86,15 +88,15 @@ def DM_number_original(m_dm,e_per_nu,start,end,n_total):
     
 
 def Spectrum(m_dm,e_per_nu,start,end,n_total):
-    gamma = Kinematics.energy_kicked_by_neutrino(E_per_nu, M_nu,m_dm)/m_dm
-    beta = (1-gamma**(-2))**0.5
+    beta = (E_per_nu**2- M_nu**2)**0.5/(E_per_nu+M_DM)
+    gamma = (1-beta**(2))**0.5  
     time_delay = np.sum((start-end)**2)**0.5*(1/beta-1)/(3e10)
-    #print("time delay:"+str(time_delay))
+    
 
     R = (np.sum((start-end)**2))**0.5
     l = end -start
     def dL_dEdR(r,theta):
-        #theta = mt.acos((E/ (M_DM/(1-beta**2)) -1)/(beta**2))
+        
         dE_dtheta = M_DM *(beta**2)*mt.sin(theta)/(1-beta**2)
         def f(r):
             psi = theta - mt.asin(r/R*mt.sin(theta))
@@ -103,16 +105,18 @@ def Spectrum(m_dm,e_per_nu,start,end,n_total):
             sec = 1/mt.cos(theta)
             dpsi_dtheta = 1- r/R*mt.cos(theta) /(1-(r/R*mt.sin(theta))**2)**0.5
             dn_domega= (sec**3)*(1-beta*beta)/((sec**2-beta**2)**2)/np.pi
-        
+            
             return mt.sin(psi)*cos_theta_psi*R /(R**2+r**2-2*R*r*mt.cos(psi))*dn_domega*dpsi_dtheta/(x*(1+x)*(1+x))
-        return  f(r)#/dE_dtheta #integrate.nquad(f, [[0,R]])[0]  /dE_dtheta
+        return  f(r)
 
+    
     E_0 = M_DM*1.001
     E_f = M_DM/(1-beta**2)*(1+(beta**2))*0.999
+    
     norm = integrate.dblquad(dL_dEdR, 0, np.pi/2, lambda theta: 0, lambda theta: R)[0]*2*np.pi  
-
+    print(norm)
     def dL_dE(theta):
-        #theta = mt.acos((E/ (M_DM/(1-beta**2)) -1)/(beta**2))
+        
         sec = 1/mt.cos(theta)
         dE_dtheta = M_DM *(beta**2)/(1-beta**2) *4*(gamma**2)*mt.tan(theta)*(sec**2) /(1+(gamma**2)*(mt.tan(theta)**2))
         def f(r):
@@ -125,21 +129,24 @@ def Spectrum(m_dm,e_per_nu,start,end,n_total):
         
             return mt.sin(psi)*cos_theta_psi*R /(R**2+r**2-2*R*r*mt.cos(psi))*dn_domega*dpsi_dtheta/(x*(1+x)*(1+x))
         return  integrate.nquad(f, [[0,R]])[0]  /dE_dtheta
-
+    
     theta =np.linspace(0.001,np.pi/2,1000)
-    E = [M_DM/(1-beta**2)*(1+(beta**2)*np.cos(theta[i]))for i in range(0,1000)]
+    E = [M_DM/(1-beta**2)*(1+(beta**2)*(1-(gamma**2)*(mt.tan(theta[i])**2)) /(1+(gamma**2)*(mt.tan(theta[i])**2)))for i in range(0,1000)]
+    print()
     spec = [dL_dE(theta[i])/norm for i in range(0,1000)]
    
     plt.plot(E, spec, color ='blue')
     plt.xlabel('E(eV)')
     plt.ylabel('dL_chi/dE(1/cm**2 eV)')
-    plt.legend(loc='upper right')
+    
     plt.show()
     
 
 if __name__== '__main__':
-    gamma = Kinematics.energy_kicked_by_neutrino(E_per_nu, M_nu,M_DM)/M_DM
-    beta = (1-gamma**(-2))**0.5    
+    
+    beta = (E_per_nu**2- M_nu**2)**0.5/(E_per_nu+M_DM)
+    gamma = (1-beta**(2))**0.5      
+    print(beta)
     print("Angle Norm:"+str(angle_norm(beta,0.5,1)))
 
     start=np.array([0.87*3.08567758e21,0,2.4*3.08567758e18])
